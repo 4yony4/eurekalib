ee60ce88bfe9a159003780d1c60abdaa249e9de1
MRGA7 <mariaagon7@gmail.com>
2022-10-18 08:45:53 +0200

    Añadido view Maria

---
+++ lib/custom_views/MariaView.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+class YonyView extends StatelessWidget{
+
+
+
+  const YonyView({Key? key}) : super(key:key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Maria View'),
+      ),
+      //backgroundColor: Colors.orangeAccent,
+      body: Center(
+          child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_njkcOMQ9Otm0AFpmrbQRVKsyGbcZXwq6jSQ2fTb0tw&s")
+      ),
+    );
+  }
+
+}
