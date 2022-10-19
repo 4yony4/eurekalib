82736963fcee8e0c5099c22841c6ed46a8b0d122
Gabriel Rotariu <dam.gabriel.rotariu@gmail.com>
2022-10-19 10:49:54 +0200

    Prueba GabrielRotariu

---
+++ lib/custom_views/GabiVIew.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GabiView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Prueba"),
+      ),
+      body: Column(),
+
+    );
+  }
+}
