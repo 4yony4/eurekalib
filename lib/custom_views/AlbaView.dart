58482acf841589851d2a3c71bb070188d12a6575
albrox9 <dam.albrox9@gmail.com>
2022-10-24 13:51:30 +0200

    Commit_Alba

---
+++ lib/custom_views/AlbaView.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AlbaView extends StatelessWidget{
+
+
+
+  const AlbaView({Key? key}) : super(key:key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alba view'),
+      ),
+      //backgroundColor: Colors.orangeAccent,
+      body: Center(
+          child: Image.network("https://cdnb.20m.es/yaestaellistoquetodolosabe/files/2014/05/Por-qu%C3%A9-al-planeta-Tierra-tambi%C3%A9n-se-le-llama-%E2%80%98mundo%E2%80%99.jpg")
+      ),
+    );
+  }
+
+}
