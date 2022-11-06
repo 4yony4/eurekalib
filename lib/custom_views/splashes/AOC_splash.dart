7b080c78b731bfc6c8813981304075ea5de434e7
Alejandro Oliva <alex.olivacorreia02@gmail.com>
2022-11-06 19:51:11 +0100

    AOC_splash finalizado y README.md actualizado

--- lib/custom_views/splashes/AOC_splash.dart
+++ lib/custom_views/splashes/AOC_splash.dart
@@ -1,98 +1,101 @@
 import 'package:flutter/material.dart';
 
-class SplashView extends StatefulWidget {
+class AOC_splash extends StatefulWidget {
   final Function redirection;
+  final Color? backgroundColor;
+  final Color? loadingCircleBackgroundColor;
+  final Color loadingCircleFirstColor;
+  final Color? loadingCircleSecondColor;
+  final String imagePath;
+  final double width;
+  final double height;
+  final int? primaryColorDuration;
+  final int? secondaryColorDuration;
+  final double? circleStrokeWidth;
+  final bool? isAsset;
 
-  const SplashView(
-      {super.key, required this.redirection});
+  const AOC_splash(
+      {super.key,
+      required this.redirection,
+      this.backgroundColor,
+      this.loadingCircleBackgroundColor,
+      required this.loadingCircleFirstColor,
+      this.loadingCircleSecondColor,
+      required this.imagePath,
+      required this.width,
+      required this.height,
+      this.primaryColorDuration = 600,
+      this.secondaryColorDuration = 600,
+      this.circleStrokeWidth = 4.0,
+      this.isAsset = true});
 
   @override
   State<StatefulWidget> createState() {
-    return _SplashViewState();
+    return _AOC_splash();
   }
 }
 
-class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<dynamic> _colorTween;
+class _AOC_splash extends State<AOC_splash> with TickerProviderStateMixin {
+  late Animation<Color?>? _colorTween;
+  late AnimationController _animationController;
+  late Image image;
 
   @override
   void initState() {
-    _colorTween = controller
-        .drive(ColorTween(begin: Colors.yellow, end: Colors.deepPurple));
-    controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 5),
-    )..addListener(() {
-        setState(() {});
-      });
-    controller.repeat();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: widget.secondaryColorDuration!),
+        reverseDuration: Duration(milliseconds: widget.primaryColorDuration!));
+    _colorTween = _animationController.drive(ColorTween(
+        begin: widget.loadingCircleFirstColor,
+        end: widget.loadingCircleSecondColor));
+    _animationController.repeat(reverse: true);
+    image = widget.isAsset!
+        ? Image.asset(widget.imagePath)
+        : Image.network(widget.imagePath);
     super.initState();
-    WidgetsBinding.instance
-        .addPostFrameCallback((timeStamp) => widget.redirection);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      widget.redirection.call();
+    });
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(24, 24, 24, 100),
-      body: Container(
-        child: CircularProgressIndicator(
-          valueColor: _colorTween.value,
-          value: controller.value,
-          semanticsLabel: 'Circular progress indicator',
+      backgroundColor: widget.backgroundColor,
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              height: widget.height,
+              width: widget.width,
+              child: ClipOval(
+                child: image,
+              ),
+            ),
+            Container(
+              height: widget.height,
+              width: widget.width,
+              child: CircularProgressIndicator(
+                strokeWidth: widget.circleStrokeWidth!,
+                valueColor: widget.loadingCircleSecondColor != null
+                    ? _colorTween
+                    : AlwaysStoppedAnimation(widget.loadingCircleFirstColor),
+                backgroundColor: widget.loadingCircleBackgroundColor,
+                semanticsLabel: 'Circular progress indicator',
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-/*
-  void redirection() async {
-    Profile? userProfile;
-    Future.delayed(const Duration(milliseconds: 600));
-
-    if (!Auth_management().isLogged()) {
-      Navigator.popAndPushNamed(context, '/login');
-      return;
-    }
-
-    DBprofile()
-        .getUserProfile(FirebaseAuth.instance.currentUser!.uid.toString())
-        .then((value) {
-      userProfile = value;
-
-      if (userProfile?.birthday == null || userProfile?.country == null) {
-        Navigator.of(context).popAndPushNamed('/on-boarding');
-        return;
-      }
-
-      Navigator.of(context).popAndPushNamed('/home');
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      redirection();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(24, 24, 24, 100),
-      body: Container(
-        child: const SpinKitWave(
-          color: Colors.deepPurple,
-          size: 80.0,
-        ),
-      ),
-    );
-  }*/
 }
--- lib/custom_views/splashes/README.md
+++ lib/custom_views/splashes/README.md
@@ -8,17 +8,85 @@ Estas vistas se utilizarán como pantalla de bienvenida al usuario.
 Estas son las distintas clases que hay:
 
 ```dart
-//TODO: Add the classes inside the folder with some explanation.
+AOC_splash.dart
+
+class AOC_splash extends StatefulWidget {
+  final Function redirection;
+  final Color? backgroundColor;
+  final Color? loadingCircleBackgroundColor;
+  final Color loadingCircleFirstColor;
+  final Color? loadingCircleSecondColor;
+  final String imagePath;
+  final double width;
+  final double height;
+  final int? primaryColorDuration;
+  final int? secondaryColorDuration;
+  final double? circleStrokeWidth;
+  final bool? isAsset;
+
+  const AOC_splash(
+      {super.key,
+        required this.redirection,
+        this.backgroundColor,
+        this.loadingCircleBackgroundColor,
+        required this.loadingCircleFirstColor,
+        this.loadingCircleSecondColor,
+        required this.imagePath,
+        required this.width,
+        required this.height,
+        this.primaryColorDuration = 600,
+        this.secondaryColorDuration = 600,
+        this.circleStrokeWidth = 4.0,
+        this.isAsset = true});
+}
 ```
 
 ##Preview
 
-```dart
-//TODO: Add images to help the user what to choose
-```
+###AOC_splash
+
+![Alt Text](https://firebasestorage.googleapis.com/v0/b/fluterchat-977e2.appspot.com/o/AOC_splash_gif.gif?alt=media&token=00205859-5266-4c23-9d35-2a6c49d45225)
 
 ##Usage
 
+###AOC_splash
+
 ```dart
-//TODO: Add examples of how to use the classes.
+import 'package:eurekalib/custom_views/splashes/AOC_splash.dart';
+Widget build(BuildContext context) {
+  return AOC_splash(
+      isAsset: false, //Opcional. Un booleano para confirmar si el path de la imagen es un asset 
+                      // o una url de internet. Por defecto es true.
+      
+      redirection: () => redirection(context), //Obligatorio. Función de redirección 
+      
+      primaryColorDuration: 400, //Opcional. La duración en milisegundos del color primario 
+                                 // en la animación de cambio de colores. 
+                                 // Por defecto son 600 ms.
+      
+      secondaryColorDuration: 500, //Opcional. La duración en milisegundos del color secundario en 
+                                   // la animación de cambio de colores. 
+                                   // Por defecto son 600 ms.
+      
+      circleStrokeWidth: 12, //Opcional. El ancho de la linea del círculo de carga. 
+                             // Por defecto es 4.0.
+      
+      backgroundColor: Color.fromRGBO(220, 220, 220, 0), //Opcional. El color de fondo de la vista. 
+      
+      loadingCircleBackgroundColor: Colors.blueGrey, //Opcional. El color de fondo del círculo de carga.
+      
+      loadingCircleFirstColor: Colors.yellow, //Obligatorio. El color principal del círculo de carga.
+      
+      loadingCircleSecondColor: Colors.purple, // Opcional. El color al que se cambia el círculo.
+                                               // Al dejarlo vacío el circulo no cambia de color. 
+      
+      width: 170, //Obligatorio. El ancho de la imagen y el círculo de carga.
+      
+      height: 170, //Obligatorio. El alto de la imagen y el círculo de carga.
+      
+      imagePath: //Obligatorio. La ruta de la imagen.
+      "https://media.istockphoto.com/id/1209654046/vector/user-avatar-profile-icon-black-vector"
+          "-illustration.jpg?s=612x612&w=0&k=20&c=EOYXACjtZmZQ5IsZ0UUp1iNmZ9q2xl1BD1VvN6tZ2UI=");
+}
+ 
 ```
