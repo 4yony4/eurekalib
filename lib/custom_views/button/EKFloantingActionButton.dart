d75f97a2f98ef99f646ea2d82f51ae6b3dae08f3
caroe <caroelena99@gmail.com>
2022-11-15 12:55:09 +0100

    Clase Buttons Carolina

---
+++ lib/custom_views/button/EKFloantingActionButton.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FabExample(),
+    );
+  }
+}
+
+class FabExample extends StatelessWidget {
+  const FabExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FloatingActionButton Sample'),
+      ),
+      body: const Center(child: Text('Press the button below!')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.navigation),
+      ),
+    );
+  }
+}
